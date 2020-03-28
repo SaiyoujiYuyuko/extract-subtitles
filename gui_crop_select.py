912f09aabc794da54361b02837516b33dfd6029f
duangsuse <fedora-opensuse@outlook.com>
2020-03-28 14:26:12 +0800

    Update gui_crop_select.py script

--- gui_crop_select.py
+++ gui_crop_select.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import Tuple, cast
+from typing import Tuple, Iterator, cast
 
 from argparse import ArgumentParser, FileType
 
@@ -13,43 +13,55 @@ from cv2 import EVENT_LBUTTONDOWN, EVENT_LBUTTONUP
 def cv2WaitKey(block_ms = 1) -> str:
   return chr(waitKey(block_ms) & 0xFF)
 
-def guiSelectionUMat(mat: UMat, title = "Rect Selection (r:replot; c:OK)", box_color = (0, 0, 0xFF)) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-  ''' (left_top, right_down) '''
-  p_lt, p_rd = None, None
-  def onMouseEvent(event, x, y, flags, param):
-    nonlocal p_lt, p_rd
-    if event == EVENT_LBUTTONDOWN:
-      p_lt = (x, y)
-    elif event == EVENT_LBUTTONUP:
-      p_rd = (x, y)
-      rectangle(shot, p_lt, p_rd, box_color, thickness=2)
-      imshow(title, shot)
-  namedWindow(title, WINDOW_NORMAL)
-  setMouseCallback(title, onMouseEvent)
-  shot = mat.copy()
-  while True:
-    imshow(title, shot)
-    key = cv2WaitKey(0)
-    if key == 'r': shot = mat.copy()
-    elif key == 'c': break
-  destroyWindow(title)
-  return cast(Tuple, (p_lt, p_rd))
-
 def rectLtrd2Xywh(lt: Tuple[int, int], rd: Tuple[int, int]) -> Tuple[int, int, int, int]:
   x1, y1 = lt
   x2, y2 = rd
   return (x1, y1, x2-x1, y2-y1)
 
-def selectCropRect(cap: VideoCapture, title = "Video (c:OK)") -> Tuple[int, int, int, int]:
+def cv2Crop(mat: UMat, ltrd) -> UMat:
+  x,y, w,h = rectLtrd2Xywh(*ltrd)
+  return mat[y:y+h, x:x+w]
+
+# == App ==
+def guiSelectionUMat(mat: UMat, title="Rect Selection (r:replot; c:OK)", box_color=(0xFF, 0x00, 0x00), thickness=3) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+  ''' (left_top, right_down) '''
+  p_lt, p_rd = None, None
+  shot = mat.copy()
+  def onMouseEvent(event, x, y, flags, param):
+    nonlocal p_lt, p_rd, shot
+    if event == EVENT_LBUTTONDOWN:
+      p_lt = (x, y)
+    elif event == EVENT_LBUTTONUP:
+      p_rd = (x, y)
+      rectangle(shot, p_lt, p_rd, box_color, thickness)
+      imshow(title, shot)
+  namedWindow(title, WINDOW_NORMAL)
+  setMouseCallback(title, onMouseEvent)
+  imshow(title, shot)
+  while True:
+    key = cv2WaitKey(0)
+    if key == 'c' and (p_lt != None and p_rd != None):
+      destroyWindow(title)
+      return cast(Tuple, (p_lt, p_rd))
+    elif key == 'r':
+      shot = mat.copy()
+      imshow(title, shot)
+
+def selectCropRects(cap: VideoCapture, title = "Video (c:OK; q:finished)", title_preview = "Preview") -> Iterator[Tuple[int, Tuple[int, int, int, int]]]:
   ''' position&size (x, y, w, h) '''
+  index = 0
+  ltrd = None
   unfinished, img = cap.read()
   while unfinished:
     imshow(title, img)
-    if cv2WaitKey() == 'c':
-      lt, rd = guiSelectionUMat(img)
-      return rectLtrd2Xywh(lt, rd)
+    if ltrd != None: imshow(title_preview, cv2Crop(img, ltrd))
+    key = cv2WaitKey()
+    if key == 'c':
+      ltrd = guiSelectionUMat(img)
+      yield (index, rectLtrd2Xywh(*ltrd))
+    elif key == 'q': break
     unfinished, img = cap.read()
-  raise EOFError("video ended, but selection isn't made")
+    index += 1
 
 app = ArgumentParser("gui_crop_select", description="Interactive video crop rect selection")
 app.add_argument("video", nargs="+", type=FileType("r"), help="video paths")
@@ -58,6 +70,7 @@ if __name__ == "__main__":
   cfg = app.parse_args()
   for path in map(lambda it: it.name, cfg.video):
     cap = VideoCapture(path)
-    t = selectCropRect(cap)
+    crops = selectCropRects(cap)
+    for i, c in crops:
+      print(f"{i} " + f"{c[0:2]}{c[2:4]}".replace(" ", ""))
     cap.release()
-    print(f"{t[0:2]}{t[2:4]}".replace(" ", ""))
