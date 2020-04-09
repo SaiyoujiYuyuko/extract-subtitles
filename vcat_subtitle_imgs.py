97d81d4d685b618c9e097b7dad96240b6117fd17
duangsuse <fedora-opensuse@outlook.com>
2020-04-09 17:14:29 +0800

    vcat_subtitle_imgs: add color FF0000 vertical strike out

--- vcat_subtitle_imgs.py
+++ vcat_subtitle_imgs.py
@@ -7,6 +7,8 @@ from PIL import Image as Pillow
 
 LTRD = NewType("LTRD", Tuple[int,int,int,int])
 
+
+
 def vConcateImages(path_imgs: Dict[str,Image]) -> Tuple[Image, Dict[str,LTRD]]:
   width = max(map(lambda it: it.width, path_imgs.values()))
   height = sum(map(lambda it: it.height, path_imgs.values()))
@@ -24,7 +26,9 @@ def vConcateImages(path_imgs: Dict[str,Image]) -> Tuple[Image, Dict[str,LTRD]]:
 
 def saveCropImages(img: Image, areas: Dict[str,LTRD]):
   for (k, v) in areas.items():
-    img.crop(v).save(k)
+    area = img.crop(v)
+    area.save(k)
+    onAreaWrote(k, area)
 
 from re import findall
 from json import dump, load
@@ -43,6 +47,31 @@ def main(args):
 
     (img, areas) = vConcateImages({path: Pillow.open(path) for path in srcs})
     img.save(dst); dump(areas, open(f"{dst}.json", "w+"))
+
+def channelHistogram(img):
+  n_channels = Pillow.getmodebands(img.mode)
+  hist = img.histogram()
+  return tuple(hist[i:i+256] for i in range(0, n_channels*256, 256))
+
+def imagePixels(img):
+  for y in range(0, img.height):
+    for x in range(0, img.width):
+      yield img.getpixel((x, y))
+
+def count(xs): return sum(map(lambda _: 1, xs))
+
+import os
+def defaultOnAreaWrote(path, img, red_value_range=range(20, 100)):
+  (r,g,b) = channelHistogram(img)[0:3]
+  redValue = r[0xFF] - (g[0xFF] + b[0xFF])
+  if redValue in red_value_range:
+    redMarks = count(filter(lambda it: it[0:3] == (0xFF,0,0), imagePixels(img)))
+    if not redMarks > 0: return
+    img.show()
+    print(f"Removing {path} (redmarks {redMarks})")
+    os.remove(path)
+
+onAreaWrote = eval("lambda path, img: " + (os.environ.get("AREA_WROTE") or "defaultOnAreaWrote(path, img)"))
 
 from sys import argv
 if __name__ == "__main__": main(argv[1:])
